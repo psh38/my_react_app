b8dd72e974156794501ac0880064f005de1d5726
psh <sosohy38@gmail.com>
2024-05-27 11:35:47 +0900

    fistcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 함수형 (최근 추세)
+// function App() {
+//   return (
+//     <div className="App">
+//       hello
+//     </div>
+//   );
+// }
+
+//클래스형
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        hello
+      </div>
+    )
+  }
 }
 
 export default App;
