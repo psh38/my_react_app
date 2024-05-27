7510054f8c6a9f79609d095ff45f8b826d27a1a6
psh <sosohy38@gmail.com>
2024-05-27 11:54:26 +0900

    Myheader 컴포넌트

--- src/App.js
+++ src/App.js
@@ -12,11 +12,29 @@ import './App.css';
 
 //클래스형
 
+// Myheader
+class Myheader extends Component {
+  render() {
+    return (
+      //주의사항 JSX문법영역 이기 때문에 부모가 반드시 있어야 함!
+      <header>
+        <h1 class="logo">React</h1>
+        <p>Single Page Application</p>
+      </header>
+      // 빈 프래그먼트 (해결방법)
+      // <>
+      //   <h1 class="logo">React</h1>
+      //   <p>Single Page Application</p>
+      // </>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hello
+        <Myheader></Myheader>
       </div>
     )
   }
